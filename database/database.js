f84bcc3281442b67dd020cb85146d2b34e79457f
araiz45 <araizzahid45@gmail.com>
2023-11-17 23:14:32 +0500

    database change

--- database/database.js
+++ database/database.js
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 export default async function main() {
-    try {
-        const db = await mongoose.connect(process.env.MONGO_URL)
-        if(db) {
-            console.log("database has been connected")
-        }
-    } catch (error) {
-        console.log("MongoDB Error " + error);   
+  try {
+    const db = await mongoose.connect(
+      process.env.MONGO_URL ||
+        "mongodb+srv://Araix:nabeera12@cluster0.d4ayc4r.mongodb.net/"
+    );
+    if (db) {
+      console.log("database has been connected");
     }
+  } catch (error) {
+    console.log("MongoDB Error " + error);
+  }
 }
