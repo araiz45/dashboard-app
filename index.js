db85d3ed7d2031016002ba9794875ef6e302efc1
araiz45 <araizzahid45@gmail.com>
2023-11-17 22:27:09 +0500

    heroku

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import express from 'express'
-import userRouter from './routes/userRoute.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import database from './database/database.js'
-import entryRoute from './routes/entryRoute.js'
+import express from "express";
+import userRouter from "./routes/userRoute.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import database from "./database/database.js";
+import entryRoute from "./routes/entryRoute.js";
 
-dotenv.config()
-database().catch(err => console.log(err.message));
+dotenv.config();
+database().catch((err) => console.log(err.message));
 const app = express();
-app.use(express.json())
-app.use(cors({ 
+app.use(express.json());
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
-app.use('/api/user' , userRouter)
-app.use('/api/entry' , entryRoute)
+  })
+);
+app.use("/api/user", userRouter);
+app.use("/api/entry", entryRoute);
 
 app.listen(5000, () => {
-    console.log("Server has been started successfully")
-})
+  console.log("Server has been started successfully");
+});
